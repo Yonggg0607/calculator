c2a63bc47dfa5d141893261897893ab2c83c7941
Yonggg0607 <yimcheayong@gmail.com>
2025-01-02 23:08:07 +0700

    Add files via upload

---
+++ Cal.js
@@ -0,0 +1,24 @@
+let currentInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    document.getElementById('display').value = currentInput;
+}
+function appendOperator(operator) {
+    currentInput += ' ' + operator + ' ';
+    document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        currentInput = eval(currentInput).toString();
+        document.getElementById('display').value = currentInput;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
